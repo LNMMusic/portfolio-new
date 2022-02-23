c9da55d9bb0fb493dfe4e94693674f688aae11e5
Lucas Masiero <lucas.masi.ero@hotmail.com>
2022-02-23 20:45:26 -0300

    structure for css

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+const Hero = () => {
+  
+
+
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Hero
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+// Components
+import Hero from '../components/Hero'
 
 
 const HomePage = () => {
@@ -6,25 +8,25 @@ const HomePage = () => {
   
     return (
         <>
-            <article class="uk-comment">
-                <header class="uk-comment-header">
-                    <div class="uk-grid-medium uk-flex-middle" uk-grid>
-                        <div class="uk-width-auto">
-                            <img class="uk-comment-avatar" src="images/avatar.jpg" width="80" height="80" alt=""/>
-                        </div>
-                        <div class="uk-width-expand">
-                            <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">Author</a></h4>
-                            <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
-                                <li><a href="#">12 days ago</a></li>
-                                <li><a href="#">Reply</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </header>
-                <div class="uk-comment-body">
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-                </div>
-            </article>
+            <div className="section-hero">
+                <Hero />
+            </div>
+
+            <div className="section-description">
+
+            </div>
+
+            <div className="section-projects">
+
+            </div>
+
+            <div className="section-experiences">
+
+            </div>
+
+            <div className="section-contact">
+                
+            </div>
         </>
     )
 }
