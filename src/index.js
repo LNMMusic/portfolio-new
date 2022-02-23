7e4bc452f86dd0a7278439ac20faf15663ffddf6
Lucas Masiero <lucas.masi.ero@hotmail.com>
2022-02-23 19:34:48 -0300

    first base react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// LIBS
+import React from 'react';  import ReactDOM from 'react-dom';
+// CSS                                                          [html head]
+import './normalize.css';   import './index.css';
+
+
+
+// RENDER                                                       [html body]
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,                            // Element  ->            App Component [w/ internal comps and css]
+  document.getElementById('root')     // Target   ->            root
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
